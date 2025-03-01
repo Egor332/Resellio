fa86bc2f297a8909637eb159c304e317d1bf36c4
Yahor Kachanouski <kegor2323@gmail.com>
2025-03-01 23:15:59 +0100

    Tests for SendGridEmailSender added, Subject being null bug fixed

--- backend/ResellioBackend/Services/Implementations/SendGridEmailSender.cs
+++ backend/ResellioBackend/Services/Implementations/SendGridEmailSender.cs
@@ -8,20 +8,25 @@ namespace ResellioBackend.Services.Implementations
     {
         private readonly string _apiKey;
         private readonly string _senderEmail;
+        public Func<ISendGridClient> ClientFactory { get; set; }
+
         public SendGridEmailSender(IConfiguration configuration)
         {
             _apiKey = configuration["SendGridParameters:SendGridKey"];
             _senderEmail = configuration["SendGridParameters:CompanyEmail"];
+
+            ClientFactory = () => new SendGridClient(_apiKey);
         }
 
         public async Task<bool> SendEmailAsync(string email, string subject, string plainTextContent, string htmlContent)
         {
-            var client = new SendGridClient(_apiKey);
+            var client = ClientFactory();
 
             var from = new EmailAddress(_senderEmail, "Resellio");
             var to = new EmailAddress(email, email);
 
             var msg = MailHelper.CreateSingleEmail(from, to, subject, plainTextContent, htmlContent);
+            msg.Subject = subject; // does not work without this line, may be because of some conflict in CreateSingleEmail
             var response = await client.SendEmailAsync(msg);
 
             return response.IsSuccessStatusCode;
