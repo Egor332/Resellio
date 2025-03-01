3129e4f249d09149e101a45fcde77c05a55e8d14
Yahor Kachanouski <kegor2323@gmail.com>
2025-03-02 00:14:09 +0100

    UsersRepository added

---
+++ backend/ResellioBackend/Repositories/Abstractions/IUsersRepository.cs
@@ -0,0 +1,14 @@
+﻿using ResellioBackend.Models.Base;
+
+namespace ResellioBackend.Repositories.Abstractions
+{
+    public interface IUsersRepository<T> where T : UserBase
+    {
+        public Task<T?> GetByIdAsync(int id);
+        public Task<T?> GetByLoginAsync(string login);
+        public Task UpdateAsync(T entity);
+        public Task AddAsync(T entity);
+        public Task DeleteAsync(T entity);
+
+    }
+}
