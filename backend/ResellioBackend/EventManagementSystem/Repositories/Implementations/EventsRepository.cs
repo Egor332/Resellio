6b326ffd40241e1dd6e4602c799d65732fbcd7bf
MegaRoboNinja <131467705+MegaRoboNinja@users.noreply.github.com>
2025-03-07 13:26:28 +0100

    Added Event Repository

--- backend/ResellioBackend/EventManagementSystem/Repositories/Implementations/EventsRepository.cs
+++ backend/ResellioBackend/EventManagementSystem/Repositories/Implementations/EventsRepository.cs
@@ -1,6 +1,28 @@
+using Microsoft.EntityFrameworkCore;
+using ResellioBackend.EventManagementSystem.Models;
+
 namespace ResellioBackend.EventManagementSystem.Repositories.Implementations;
 
 public class EventsRepository
 {
+    private readonly ResellioDbContext _context;
+    private readonly DbSet<Event> _dbSet;
     
+    public EventsRepository(ResellioDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<Event>();
+    }
+    
+    public async Task AddAsync(Event newEvent)
+    {
+        await _dbSet.AddAsync(newEvent);
+        await _context.SaveChangesAsync();
+    }
+    
+    public async Task UpdateAsync(Event eventToUpdate)
+    {
+        _dbSet.Update(eventToUpdate);
+        await _context.SaveChangesAsync();
+    }
 }
