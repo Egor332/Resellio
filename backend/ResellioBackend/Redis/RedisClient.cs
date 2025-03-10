94894b80acfa75e52765deff52d9b26fd706a375
Egor Kachanouski <kegor2323@gmail.com>
2025-03-10 15:49:06 +0100

    Redis stuf added

---
+++ backend/ResellioBackend/Redis/RedisClient.cs
@@ -0,0 +1,16 @@
+﻿using ResellioBackend.EventManagementSystem.Models.Base;
+using StackExchange.Redis;
+
+namespace ResellioBackend.Redis
+{
+    public class RedisClient : IRedisClient
+    {
+        private readonly IDatabase _redisDb;
+
+        public RedisClient(IConnectionMultiplexer connectionMultiplexer) 
+        {
+            _redisDb = connectionMultiplexer.GetDatabase();
+        }
+
+    }
+}
