b0e96f4b2071e95e618187b9da8bc822e322fdd4
Egor Kachanouski <kegor2323@gmail.com>
2025-04-09 17:45:26 +0200

    Mock of payment controller added

---
+++ backend/ResellioBackend/TicketPurchaseSystem/Controllers/PaymentController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ResellioBackend.UserManagementSystem.Statics;
+using System.Runtime.CompilerServices;
+
+namespace ResellioBackend.TicketPurchaseSystem.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaymentController : ControllerBase
+    {
+        [Authorize(Policy = AuthorizationPolicies.CustomerPolicy)]
+        [HttpPost("create-checkout-session")]
+        public async Task<IActionResult> CreateCheckoutSession()
+        {
+            return Ok();
+        }
+
+        [NonAction]
+        [HttpPost("payment-webhook")]
+        public async Task<IActionResult> PaymentWebhook()
+        {
+            return Ok();
+        }
+    }
+}
