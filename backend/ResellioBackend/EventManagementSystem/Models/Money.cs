eb2ee12114b1cc4583d836beef60ad70a2d5511d
Egor Kachanouski <kegor2323@gmail.com>
2025-04-10 15:17:38 +0200

    Money class and CurrencyCodes static class added

---
+++ backend/ResellioBackend/EventManagementSystem/Models/Money.cs
@@ -0,0 +1,8 @@
+﻿namespace ResellioBackend.EventManagementSystem.Models
+{
+    public class Money
+    {
+        public decimal Amount { get; set; }
+        public string CurrencyCode { get; set; }
+    }
+}
---
+++ backend/ResellioBackend/Statics/CurrencyCodes.cs
@@ -0,0 +1,15 @@
+﻿namespace ResellioBackend.Statics
+{
+    public static class CurrencyCodes
+    {
+        public const string USD = "USD";
+        public const string EUR = "EUR";
+        public const string GBP = "GBP";
+        public const string PLN = "PLN";
+
+        public static readonly HashSet<string> AvailableCurrencies = new()
+        {
+            USD, EUR, GBP, PLN
+        };
+    }
+}
