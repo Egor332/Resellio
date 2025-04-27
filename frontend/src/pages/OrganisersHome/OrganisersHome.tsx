affbe7681c328df068b021ebef127676933a7128
radek02 <maksymiuk.radoslaw@gmail.com>
2025-04-27 17:01:06 +0200

    Show mock events on organiser homepage

--- frontend/src/pages/OrganisersHome/OrganisersHome.tsx
+++ frontend/src/pages/OrganisersHome/OrganisersHome.tsx
@@ -1,11 +1,14 @@
 import { Box, Typography } from '@mui/material'
+import { mockedEvents } from '../../assets/MockEventList'
+import { EventList } from '../../components/EventList/EventList'
 
 function OrganisersHome() {
   return (
     <Box sx={{ textAlign: 'center', width: '100%' }}>
-      <Typography variant="h4" component="h1" gutterBottom>
-        Organiser Homepage
+      <Typography variant="h6" component="h2" gutterBottom sx={{ mt: 3, mb: 2 }}>
+        Your Events
       </Typography>
+      <EventList events={mockedEvents} />
     </Box>
   )
 }
