f402512555aba8bf462821658ab105e2cd92792f
MegaRoboNinja <131467705+MegaRoboNinja@users.noreply.github.com>
2025-05-15 14:57:34 +0200

    refactor

--- frontend/src/services/userService.ts
+++ frontend/src/services/userService.ts
@@ -1,16 +1,21 @@
 import {UserInfoDto} from "../models/dtos/UserInfoDto.ts";
 import {apiRequest} from "./httpClient.ts";
-import {apiEndpoints, getApiEndpoint} from "../assets/constants/api.ts";
+import {API_ENDPOINTS, getApiEndpoint} from "../assets/constants/api.ts";
 
 
 const userService = {
     getUserInfo: async (): Promise<UserInfoDto> => {
         try {
-            return await apiRequest(getApiEndpoint(apiEndpoints.USER_INFO));
-        } catch (error: any) {
-            throw new Error(error.message || 'Failed to fetch user info');
+            return await apiRequest(getApiEndpoint(API_ENDPOINTS.USER_INFO));
+        } 
+        catch (error) {
+            if (error instanceof Error) {
+                throw new Error(error.message || 'Failed to fetch user info');
+            } else {
+                throw new Error('Failed to fetch user info');
+            }
         }
-    },
+    }
 };
 
 export default userService;
