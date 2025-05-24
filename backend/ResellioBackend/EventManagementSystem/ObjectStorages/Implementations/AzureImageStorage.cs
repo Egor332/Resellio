653d3c57ec6b3ef6e4ec55a8773be9e8386f7038
Egor Kachanouski <kegor2323@gmail.com>
2025-05-24 17:30:11 +0200

    IImageStorage added and implemented as an Azure Storage client, appropriate env variables added, appropriate package installed

---
+++ backend/ResellioBackend/EventManagementSystem/ObjectStorages/Implementations/AzureImageStorage.cs
@@ -0,0 +1,35 @@
+﻿using Azure.Storage.Blobs;
+using ResellioBackend.EventManagementSystem.ObjectStorages.Abstractions;
+
+namespace ResellioBackend.EventManagementSystem.ObjectStorages.Implementations
+{
+    public class AzureImageStorage : IImageStorage
+    {
+        private readonly string _connectionString;
+        private readonly string _containerName;
+
+        public AzureImageStorage(IConfiguration configuration)
+        {
+            _connectionString = configuration["ObjcetStorage:ConnectionString"];
+            _containerName = configuration["ObjectStorage:ImageContainerName"];
+            if (string.IsNullOrEmpty(_connectionString) || string.IsNullOrWhiteSpace(_containerName)) 
+            {
+                throw new ArgumentException("Environment variables was not provided");
+            }
+        }
+
+        public async Task<string> UploadImageAsync(IFormFile image)
+        {
+            var blobContainerClient = new BlobContainerClient(_containerName, _connectionString);
+            var uniqueFileName = $"{Path.GetFileNameWithoutExtension(image.FileName)}_{Guid.NewGuid()}{Path.GetExtension(image.FileName)}";
+            var blobClient = blobContainerClient.GetBlobClient(uniqueFileName);
+            var memoryStream = new MemoryStream();
+            await image.CopyToAsync(memoryStream);
+            memoryStream.Position = 0;
+            await blobClient.UploadAsync(memoryStream);
+            var uri = blobClient.Uri.AbsoluteUri;
+
+            return uri;
+        }
+    }
+}
