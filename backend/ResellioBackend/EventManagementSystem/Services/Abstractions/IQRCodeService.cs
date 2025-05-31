0df6714bcaae743a38e7a17dde8c1ffb10c771fa
Egor Kachanouski <kegor2323@gmail.com>
2025-05-31 19:19:15 +0200

    Checkpoint commit

---
+++ backend/ResellioBackend/EventManagementSystem/Services/Abstractions/IQRCodeService.cs
@@ -0,0 +1,7 @@
+﻿namespace ResellioBackend.EventManagementSystem.Services.Abstractions
+{
+    public interface IQRCodeService
+    {
+        public Task<byte[]> GenerateQRCodeAsync(Guid ticketId);
+    }
+}
---
+++ backend/ResellioBackend/EventManagementSystem/Services/Implementations/QRCodeService.cs
@@ -0,0 +1,25 @@
+﻿using ResellioBackend.EventManagementSystem.Services.Abstractions;
+
+namespace ResellioBackend.EventManagementSystem.Services.Implementations
+{
+    public class QRCodeService : IQRCodeService
+    {
+        private readonly IQRCodeTemporaryCodeService _tempCodeService;
+
+        public QRCodeService(IQRCodeTemporaryCodeService tempCodeService)
+        {
+            _tempCodeService = tempCodeService;
+        }
+
+        public async Task<byte[]> GenerateQRCodeAsync(Guid ticketId)
+        {
+            var temporaryCode = await _tempCodeService.CreateAndSaveTemporaryCodeAsync();
+            var payload = new
+            {
+                TicketId = ticketId,
+                TemporaryCode = temporaryCode,
+            };
+            throw new NotImplementedException();
+        }
+    }
+}
