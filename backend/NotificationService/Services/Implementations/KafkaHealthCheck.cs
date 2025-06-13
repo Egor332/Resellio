0a299af8d6e3e84016de49f9600a5348d9a81fef
Egor Kachanouski <kegor2323@gmail.com>
2025-06-13 12:56:04 +0200

    Kafka health check added

---
+++ backend/NotificationService/Services/Implementations/KafkaHealthCheck.cs
@@ -0,0 +1,32 @@
+﻿using Confluent.Kafka;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace NotificationService.Services.Implementations
+{
+    public class KafkaHealthCheck : IHealthCheck
+    {
+        private readonly string _bootstrapServers;
+
+        public KafkaHealthCheck(IConfiguration configuration)
+        {
+            _bootstrapServers = configuration["Kafka:BootstrapServers"];
+        }
+
+        public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var config = new ProducerConfig { BootstrapServers = _bootstrapServers };
+
+                using var producer = new ProducerBuilder<Null, string>(config).Build();
+                producer.InitTransactions(TimeSpan.FromSeconds(1));
+
+                return Task.FromResult(HealthCheckResult.Healthy("Kafka is reachable."));
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(HealthCheckResult.Unhealthy("Kafka is not reachable.", ex));
+            }
+        }
+    }
+}
