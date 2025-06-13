a844d87125746c7fe225af5bdf57ebb773263818
Egor Kachanouski <kegor2323@gmail.com>
2025-06-13 12:23:22 +0200

    Test in production notification 1

---
+++ backend/NotificationService/Controllers/IndexController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NotificationService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IndexController : ControllerBase
+    {
+        [HttpGet("check")]
+        public async Task<IActionResult> Check()
+        {
+            return Ok(new { Message = "Check" });
+        }
+    }
+}
